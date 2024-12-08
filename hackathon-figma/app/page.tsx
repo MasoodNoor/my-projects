d8954dcac06d0f3322ca10b2eb4bc976cdb81bc3
MasoodNoor <themasoodnoor100@gmail.com>
2024-12-08 21:49:25 +0500

    resolve deployment issues

--- hackathon-figma/app/page.tsx
+++ hackathon-figma/app/page.tsx
@@ -1,19 +1,9 @@
-
-
-import { Plus_Jakarta_Sans } from "next/font/google";
 import Hero from "@/components/hero";
-
-
-const plusJakartaSans = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  weight: ["600"], // Include the weight you need
-});
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      
     </main>
   );
 }
