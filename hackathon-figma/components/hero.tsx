364e24e5f4ec7e4af1ba9c5181a9aeb8d4941e07
MasoodNoor <themasoodnoor100@gmail.com>
2024-12-09 23:48:10 +0500

    setting up

--- hackathon-figma/components/hero.tsx
+++ hackathon-figma/components/hero.tsx
@@ -1,64 +1,50 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { Plus_Jakarta_Sans } from "next/font/google";
 import Pickswitchdrop from "./pickswitchdrop";
 import PopularCar from "./popularCar";
 
-const plusJakartaSans = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  weight: ["600"], // Include the weight you need
-});
-
 function Hero() {
   return (
-    <main className=" bg-[#F6F7F9] pt-[32px] px-[64px]">
-      <div className="flex gap-[32px]">
-        <div className="w-[640px] h-[360px]  rounded-[10px] bg-[#54A6FF]">
-          <div className="w-[290px]  pt-[24px] pl-[24px] flex flex-col gap-[20px]">
-            <div
-              className={`${plusJakartaSans.className} h-[96px] text-[32px] font-semibold leading-[48px] tracking-[-0.03em] text-left text-[#FFFFFF]`}
-            >
+    <main className="bg-[#F6F7F9]  px-[24px] sm:pt-[32px] sm:px-[64px]">
+      <div className="flex sm:gap-[32px]">
+        <div className="w-[327px] h-[232px] sm:py-2 sm:w-[640px] sm:h-[360px] rounded-[10px] bg-[#54A6FF] -mt-[100px] sm:-mt-0">
+          <div className="w-[240px] h-[70px] py-[16px] px-[16px] sm:w-[284px] sm:h-[224px] sm:pt-[24px] sm:pl-[24px] flex flex-col items-start gap-[12px] sm:gap-[16px]">
+            <p className=" font-plusJakarta w-[240px] h-[20px] sm:w-[272px] sm:h-[96px] text-[16px] sm:text-[32px] font-semibold leading-[24px] sm:leading-[48px] tracking-[-0.03em] text-left text-[#FFFFFF]">
               The Best Platform for Car Rental
-            </div>
-            <p
-              className={`${plusJakartaSans.className} w-full text-[16px] font-medium leading-[24px] tracking-[-0.02em] text-left text-[#FFFFFF]`}
-            >
+            </p>
+            <p className="font-plusJakarta w-[216px] h-[38px] sm:w-[284px] sm:h-[48px] text-[12px] sm:text-[16px] font-medium leading-[19.2px] sm:leading-[24px] tracking-[-0.02em] text-left text-[#FFFFFF]">
               Ease of doing a car rental safely and reliably. Of course at a low
               price.
             </p>
-            <Button
-              className={`${plusJakartaSans.className} w-[120px] h-[44px] px-[20px] py-0  rounded-[4px] bg-[#3563E9] text-white`}
-            >
-              Rental Car
-            </Button>
+            <div>
+              <Button className="font-plusJakarta w-[128px] h-[44px] p-[20px]  sm:w-[120px] sm:h-[44px] px-[20px] sm:py-0  rounded-[4px] bg-[#3563E9] text-white inline-flex items-center justify-center">
+                <p className="sm:w-[80px] sm:h-[24px] s:h-text-[12px] sm:text-[16px] font-semibold ">Rental Car</p>
+              </Button>
+            </div>
           </div>
-          <Image
-            className="ml-[137px] -mt-[70px]"
-            src="/car-images/carimg-1.png" // Image path relative to the 'public' folder
-            alt="car image" // Descriptive alt text for the image
-            width={406} // Set the width of the image (smaller than the parent to fit inside)
-            height={116} // Set the height of the image
-            // Optional: makes the image rounded (for a circular effect)
-          />
+          <div className="">
+            <Image
+              className="w-[196px] sm:w-[406px] ml-[60px] mt-[65px] sm:ml-[140px] sm:-mt-[50px]"
+              src="/car-images/carimg-1.png" // Image path relative to the 'public' folder
+              alt="car image" // Descriptive alt text for the image
+              width={406} // Set the width of the image (smaller than the parent to fit inside)
+              height={116} // Set the height of the image
+              // Optional: makes the image rounded (for a circular effect)
+            />
+          </div>
         </div>
-        <div className="w-[640px] h-[360px]  rounded-[10px] bg-[#3563E9]">
+        <div className="w-[640px] h-[360px]  rounded-[10px] bg-[#3563E9] hidden sm:block">
           <div className="w-[290px]  pt-[24px] pl-[24px] flex flex-col gap-[20px]">
-            <div
-              className={`${plusJakartaSans.className} h-[96px] text-[32px] font-semibold leading-[48px] tracking-[-0.03em] text-left text-[#FFFFFF]`}
-            >
+            <div className="font-plusJakarta h-[96px] text-[32px] font-semibold leading-[48px] tracking-[-0.03em] text-left text-[#FFFFFF]">
               Easy way to rent a car at a low price
             </div>
-            <p
-              className={`${plusJakartaSans.className} w-full text-[16px] font-medium leading-[24px] tracking-[-0.02em] text-left text-[#FFFFFF]`}
-            >
+            <p className="font-plusJakarta w-full text-[16px] font-medium leading-[24px] tracking-[-0.02em] text-left text-[#FFFFFF]">
               Providing cheap car rental services and safe and comfortable
               facilities.
             </p>
-            <Button
-              className={`${plusJakartaSans.className} w-[120px] h-[44px] px-[20px] py-0  rounded-[4px] bg-[#54A6FF] text-white`}
-            >
-              Rental Car
-            </Button>
+            <Button className="font-plusJakarta w-[128px] h-[44px] p-[20px]  sm:w-[120px] sm:h-[44px] sm:py-0  rounded-[4px] bg-[#54A6FF] text-white inline-flex items-center justify-center">
+                <p className="sm:w-[80px] sm:h-[24px] s:h-text-[12px] sm:text-[16px] font-semibold ">Rental Car</p>
+              </Button>
           </div>
           <Image
             className="ml-[180px] -mt-[90px]"
--- hackathon-figma/components/pickswitchdrop.tsx
+++ hackathon-figma/components/pickswitchdrop.tsx
@@ -8,109 +8,112 @@ const plusJakartaSans = Plus_Jakarta_Sans({
 
 function Pickswitchdrop() {
   return (
-    <main className="flex items-center justify-around py-[32px]">
-      <div className="h-[132px] rounded-[10px] pt-[32px] px-[48px] bg-white">
-        <div className="w-[92px] h-[25px] flex items-center gap-2 pb-3  ">
-          <div className="w-4 h-4 p-[4px] rounded-[70px] bg-[#3563E94D]">
-            <div className="w-2 h-2 p-[2px] rounded-[70px] bg-[#3563E9]"></div>
-          </div>
-          <p
-            className={`${plusJakartaSans.className} inline-flex items-center w-[68px] h-[20px] text-[16px] font-semibold leading-[24px] tracking-[-0.02em] text-[#1A202C]`}
-          >
-            Pick - Up
-          </p>
-        </div>
-        <div className="w-[486px] h-[48px] flex  justify-between items-center">
-          <div className="flex flex-col gap-2">
-            <p
-              className={`${plusJakartaSans.className} w-[77px] h-[20px] text-[16px] font-bold leading-[24px] tracking-[-0.02em] text-[#1A202C]`}
-            >
-              Locations
-            </p>
-            <div className="flex items-center">
-              <p
-                className={`${plusJakartaSans.className} inline-flex items-center w-[126px] h-[20px] text-[12px] font-medium leading-[24px] tracking-[-0.02em] text-[#90A3BF] `}
-              >
-                Select your city
-              </p>
-              <span className="w-[14px] h-[14px] inline-flex items-center">
-                <svg
-                  width="12"
-                  height="7"
-                  viewBox="0 0 12 7"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M6.00038 5.79939C5.59204 5.79939 5.18371 5.64189 4.87454 5.33272L1.07121 1.52939C0.902044 1.36022 0.902044 1.08022 1.07121 0.911055C1.24038 0.741888 1.52038 0.741888 1.68954 0.911055L5.49288 4.71439C5.77288 4.99439 6.22788 4.99439 6.50788 4.71439L10.3112 0.911055C10.4804 0.741888 10.7604 0.741888 10.9295 0.911055C11.0987 1.08022 11.0987 1.36022 10.9295 1.52939L7.12621 5.33272C6.81705 5.64189 6.40871 5.79939 6.00038 5.79939Z"
-                    fill="#1A202C"
-                    stroke="#1A202C"
-                    stroke-width="0.5"
-                  />
-                </svg>
-              </span>
+    <main className="flex flex-col sm:flex-row justify-center items-center sm:items-center sm:justify-around py-[32px]">
+      <div className="w-[327px] h-[120px] sm:w-[582px] sm:h-[132px] rounded-[10px] sm:rounded-[10px] sm:pt-[32px] sm:px-[48px] bg-white -ml-6 sm:-ml-0">
+        <div className="w-[285px] h-[88px] p-[16px] sm:p-[0]">
+          <div className="w-[92px] h-[25px] flex flex-row  gap-2 pb-3 ">
+            <div className="w-4 h-4 p-[4px] rounded-[70px] bg-[#3563E94D]">
+              <div className="w-2 h-2 p-[2px] rounded-[70px] bg-[#3563E9]"></div>
             </div>
-          </div>
-          <hr className="w-[1px] h-[48px] border border-[#13131329] " />
-          <div className="flex flex-col gap-2">
             <p
-              className={`${plusJakartaSans.className} w-[77px] h-[20px] text-[16px] font-bold leading-[24px] tracking-[-0.02em] text-[#1A202C] `}
+              className={`${plusJakartaSans.className} inline-flex items-center w-[68px] h-[20px] text-[16px] font-semibold leading-[24px] tracking-[-0.02em] text-[#1A202C]`}
             >
-              Date
+              Pick - Up
             </p>
-            <div className="flex items-center">
+          </div>
+          <div className="w-[295px] h-[44px] sm:w-[486px] sm:h-[48px] flex sm:justify-between">
+            <div className="flex flex-col">
               <p
-                className={`${plusJakartaSans.className} inline-flex items-center w-[126px] h-[20px] text-[12px] font-medium leading-[24px] tracking-[-0.02em] text-[#90A3BF] `}
+                className={`${plusJakartaSans.className} w-[77px] h-[20px] text-[16px] font-bold leading-[24px] tracking-[-0.02em] text-[#1A202C]`}
               >
-                Select your date
+                Locations
               </p>
-              <span className="w-[14px] h-[14px] inline-flex items-center">
-                <svg
-                  width="12"
-                  height="7"
-                  viewBox="0 0 12 7"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M6.00038 5.79939C5.59204 5.79939 5.18371 5.64189 4.87454 5.33272L1.07121 1.52939C0.902044 1.36022 0.902044 1.08022 1.07121 0.911055C1.24038 0.741888 1.52038 0.741888 1.68954 0.911055L5.49288 4.71439C5.77288 4.99439 6.22788 4.99439 6.50788 4.71439L10.3112 0.911055C10.4804 0.741888 10.7604 0.741888 10.9295 0.911055C11.0987 1.08022 11.0987 1.36022 10.9295 1.52939L7.12621 5.33272C6.81705 5.64189 6.40871 5.79939 6.00038 5.79939Z"
-                    fill="#1A202C"
-                    stroke="#1A202C"
-                    stroke-width="0.5"
-                  />
-                </svg>
-              </span>
+
+              <p
+                className={`${plusJakartaSans.className} inline-flex items-center gap-2 sm:gap-7  w-[126px] sm:w-[130px] h-[20px] text-[12px] font-medium leading-[24px] tracking-[-0.02em] text-[#90A3BF]`}
+              >
+                Semarang
+                <span className="w-[14px] h-[14px] inline-flex items-center">
+                  <svg
+                    width="12"
+                    height="7"
+                    viewBox="0 0 12 7"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M6.00038 5.79939C5.59204 5.79939 5.18371 5.64189 4.87454 5.33272L1.07121 1.52939C0.902044 1.36022 0.902044 1.08022 1.07121 0.911055C1.24038 0.741888 1.52038 0.741888 1.68954 0.911055L5.49288 4.71439C5.77288 4.99439 6.22788 4.99439 6.50788 4.71439L10.3112 0.911055C10.4804 0.741888 10.7604 0.741888 10.9295 0.911055C11.0987 1.08022 11.0987 1.36022 10.9295 1.52939L7.12621 5.33272C6.81705 5.64189 6.40871 5.79939 6.00038 5.79939Z"
+                      fill="#1A202C"
+                      stroke="#1A202C"
+                      stroke-width="0.5"
+                    />
+                  </svg>
+                </span>
+              </p>
             </div>
-          </div>
-          <hr className="w-[1px] h-[48px] border border-[#13131329] " />
-          <div className="flex flex-col gap-2">
-            <p
-              className={`${plusJakartaSans.className} w-[77px] h-[20px] text-[16px] font-bold leading-[24px] tracking-[-0.02em] text-[#1A202C] `}
-            >
-              Time
-            </p>
-            <div className="flex items-center">
+            <span className="w-[1px] h-[48px] border border-[#13131329] -ml-8 sm:-m-0"></span>
+
+            <div className="flex flex-col pl-2 sm:pl-0 ">
               <p
-                className={`${plusJakartaSans.className} inline-flex items-center w-[126px] h-[20px] text-[12px] font-medium leading-[24px] tracking-[-0.02em] text-[#90A3BF] `}
+                className={`${plusJakartaSans.className} w-[77px] h-[20px] text-[16px] font-bold leading-[24px] tracking-[-0.02em] pl-text-[#1A202C] `}
               >
-                Select your Time
+                Date
               </p>
-              <span className="w-[14px] h-[14px] inline-flex items-center">
-                <svg
-                  width="12"
-                  height="7"
-                  viewBox="0 0 12 7"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+              <div className="inline-flex item-center">
+                <p
+                  className={`${plusJakartaSans.className} inline-flex items-center gap-2 sm:gap-7  w-[126px] sm:w-[130px] h-[20px] text-[12px] font-medium leading-[24px] tracking-[-0.02em] text-[#90A3BF]`}
                 >
-                  <path
-                    d="M6.00038 5.79939C5.59204 5.79939 5.18371 5.64189 4.87454 5.33272L1.07121 1.52939C0.902044 1.36022 0.902044 1.08022 1.07121 0.911055C1.24038 0.741888 1.52038 0.741888 1.68954 0.911055L5.49288 4.71439C5.77288 4.99439 6.22788 4.99439 6.50788 4.71439L10.3112 0.911055C10.4804 0.741888 10.7604 0.741888 10.9295 0.911055C11.0987 1.08022 11.0987 1.36022 10.9295 1.52939L7.12621 5.33272C6.81705 5.64189 6.40871 5.79939 6.00038 5.79939Z"
-                    fill="#1A202C"
-                    stroke="#1A202C"
-                    stroke-width="0.5"
-                  />
-                </svg>
-              </span>
+                  20/July/2022
+                  <span className="w-[14px] h-[14px] inline-flex items-center">
+                    <svg
+                      width="12"
+                      height="7"
+                      viewBox="0 0 12 7"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M6.00038 5.79939C5.59204 5.79939 5.18371 5.64189 4.87454 5.33272L1.07121 1.52939C0.902044 1.36022 0.902044 1.08022 1.07121 0.911055C1.24038 0.741888 1.52038 0.741888 1.68954 0.911055L5.49288 4.71439C5.77288 4.99439 6.22788 4.99439 6.50788 4.71439L10.3112 0.911055C10.4804 0.741888 10.7604 0.741888 10.9295 0.911055C11.0987 1.08022 11.0987 1.36022 10.9295 1.52939L7.12621 5.33272C6.81705 5.64189 6.40871 5.79939 6.00038 5.79939Z"
+                        fill="#1A202C"
+                        stroke="#1A202C"
+                        stroke-width="0.5"
+                      />
+                    </svg>
+                  </span>
+                </p>
+              </div>
+            </div>
+            <span className="w-[1px] h-[48px] border border-[#13131329] -ml-4  sm:p-0 sm:-m-0"></span>
+
+            <div className="flex flex-col pl-2 sm:pl-0">
+              <p
+                className={`${plusJakartaSans.className} w-[77px] h-[20px] text-[16px] font-bold leading-[24px] tracking-[-0.02em] text-[#1A202C] `}
+              >
+                Time
+              </p>
+              <div className="inline-flex item-center">
+                <p
+                  className={`${plusJakartaSans.className} inline-flex items-center gap-2 sm:gap-7  w-[126px] sm:w-[130px] h-[20px] text-[12px] font-medium leading-[24px] tracking-[-0.02em] text-[#90A3BF]`}
+                >
+                  07:00
+                  <span className="w-[14px] h-[14px] inline-flex items-center">
+                    <svg
+                      width="12"
+                      height="7"
+                      viewBox="0 0 12 7"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M6.00038 5.79939C5.59204 5.79939 5.18371 5.64189 4.87454 5.33272L1.07121 1.52939C0.902044 1.36022 0.902044 1.08022 1.07121 0.911055C1.24038 0.741888 1.52038 0.741888 1.68954 0.911055L5.49288 4.71439C5.77288 4.99439 6.22788 4.99439 6.50788 4.71439L10.3112 0.911055C10.4804 0.741888 10.7604 0.741888 10.9295 0.911055C11.0987 1.08022 11.0987 1.36022 10.9295 1.52939L7.12621 5.33272C6.81705 5.64189 6.40871 5.79939 6.00038 5.79939Z"
+                        fill="#1A202C"
+                        stroke="#1A202C"
+                        stroke-width="0.5"
+                      />
+                    </svg>
+                  </span>
+                </p>
+              </div>
             </div>
           </div>
         </div>
@@ -155,7 +158,7 @@ function Pickswitchdrop() {
           </svg>
         </button>
       </div>
-      <div className="h-[132px] rounded-[10px] pt-[32px] px-[48px] bg-white ">
+      {/* <div className="h-[132px] rounded-[10px] pt-[32px] px-[48px] bg-white ">
         <div className="w-[120px] h-[25px] flex items-center gap-2 pb-3 ">
           <div className="w-4 h-4 p-[4px] rounded-[70px] bg-[#3563E94D]">
             <div className="w-2 h-2 p-[2px] rounded-[70px] bg-[#3563E9]"></div>
@@ -260,11 +263,118 @@ function Pickswitchdrop() {
             </div>
           </div>
         </div>
+      </div> */}
+      <div className="w-[327px] h-[120px] sm:w-[582px] sm:h-[132px] rounded-[10px] sm:rounded-[10px] sm:pt-[32px] sm:px-[48px] bg-white -ml-6 sm:-ml-0">
+        <div className="w-[285px] h-[88px] p-[16px] sm:p-[0]">
+          <div className="w-[140px] h-[25px] flex flex-row  gap-2 pb-3 ">
+            <div className="w-4 h-4 p-[4px] rounded-[70px] bg-[#3563E94D]">
+              <div className="w-2 h-2 p-[2px] rounded-[70px] bg-[#3563E9]"></div>
+            </div>
+            <p
+              className={`${plusJakartaSans.className} inline-flex items-center w-[100px] h-[20px] text-[16px] font-semibold leading-[24px] tracking-[-0.02em] text-[#1A202C]`}
+            >
+              Drop - Off
+            </p>
+          </div>
+          <div className="w-[295px] h-[44px] sm:w-[486px] sm:h-[48px] flex sm:justify-between">
+            <div className="flex flex-col">
+              <p
+                className={`${plusJakartaSans.className} w-[77px] h-[20px] text-[16px] font-bold leading-[24px] tracking-[-0.02em] text-[#1A202C]`}
+              >
+                Locations
+              </p>
+
+              <p
+                className={`${plusJakartaSans.className} inline-flex items-center gap-2 sm:gap-7  w-[126px] sm:w-[130px] h-[20px] text-[12px] font-medium leading-[24px] tracking-[-0.02em] text-[#90A3BF]`}
+              >
+                Semarang
+                <span className="w-[14px] h-[14px] inline-flex items-center">
+                  <svg
+                    width="12"
+                    height="7"
+                    viewBox="0 0 12 7"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M6.00038 5.79939C5.59204 5.79939 5.18371 5.64189 4.87454 5.33272L1.07121 1.52939C0.902044 1.36022 0.902044 1.08022 1.07121 0.911055C1.24038 0.741888 1.52038 0.741888 1.68954 0.911055L5.49288 4.71439C5.77288 4.99439 6.22788 4.99439 6.50788 4.71439L10.3112 0.911055C10.4804 0.741888 10.7604 0.741888 10.9295 0.911055C11.0987 1.08022 11.0987 1.36022 10.9295 1.52939L7.12621 5.33272C6.81705 5.64189 6.40871 5.79939 6.00038 5.79939Z"
+                      fill="#1A202C"
+                      stroke="#1A202C"
+                      stroke-width="0.5"
+                    />
+                  </svg>
+                </span>
+              </p>
+            </div>
+            <span className="w-[1px] h-[48px] border border-[#13131329] -ml-8 sm:-m-0"></span>
+
+            <div className="flex flex-col pl-2 sm:pl-0 ">
+              <p
+                className={`${plusJakartaSans.className} w-[77px] h-[20px] text-[16px] font-bold leading-[24px] tracking-[-0.02em] pl-text-[#1A202C] `}
+              >
+                Date
+              </p>
+              <div className="inline-flex item-center">
+                <p
+                  className={`${plusJakartaSans.className} inline-flex items-center gap-2 sm:gap-7  w-[126px] sm:w-[130px] h-[20px] text-[12px] font-medium leading-[24px] tracking-[-0.02em] text-[#90A3BF]`}
+                >
+                  21/July/2022
+                  <span className="w-[14px] h-[14px] inline-flex items-center">
+                    <svg
+                      width="12"
+                      height="7"
+                      viewBox="0 0 12 7"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M6.00038 5.79939C5.59204 5.79939 5.18371 5.64189 4.87454 5.33272L1.07121 1.52939C0.902044 1.36022 0.902044 1.08022 1.07121 0.911055C1.24038 0.741888 1.52038 0.741888 1.68954 0.911055L5.49288 4.71439C5.77288 4.99439 6.22788 4.99439 6.50788 4.71439L10.3112 0.911055C10.4804 0.741888 10.7604 0.741888 10.9295 0.911055C11.0987 1.08022 11.0987 1.36022 10.9295 1.52939L7.12621 5.33272C6.81705 5.64189 6.40871 5.79939 6.00038 5.79939Z"
+                        fill="#1A202C"
+                        stroke="#1A202C"
+                        stroke-width="0.5"
+                      />
+                    </svg>
+                  </span>
+                </p>
+              </div>
+            </div>
+            <span className="w-[1px] h-[48px] border border-[#13131329] -ml-4  sm:p-0 sm:-m-0"></span>
+
+            <div className="flex flex-col pl-2 sm:pl-0">
+              <p
+                className={`${plusJakartaSans.className} w-[77px] h-[20px] text-[16px] font-bold leading-[24px] tracking-[-0.02em] text-[#1A202C] `}
+              >
+                Time
+              </p>
+              <div className="inline-flex item-center">
+                <p
+                  className={`${plusJakartaSans.className} inline-flex items-center gap-2 sm:gap-7  w-[126px] sm:w-[130px] h-[20px] text-[12px] font-medium leading-[24px] tracking-[-0.02em] text-[#90A3BF]`}
+                >
+                  01:00
+                  <span className="w-[14px] h-[14px] inline-flex items-center">
+                    <svg
+                      width="12"
+                      height="7"
+                      viewBox="0 0 12 7"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M6.00038 5.79939C5.59204 5.79939 5.18371 5.64189 4.87454 5.33272L1.07121 1.52939C0.902044 1.36022 0.902044 1.08022 1.07121 0.911055C1.24038 0.741888 1.52038 0.741888 1.68954 0.911055L5.49288 4.71439C5.77288 4.99439 6.22788 4.99439 6.50788 4.71439L10.3112 0.911055C10.4804 0.741888 10.7604 0.741888 10.9295 0.911055C11.0987 1.08022 11.0987 1.36022 10.9295 1.52939L7.12621 5.33272C6.81705 5.64189 6.40871 5.79939 6.00038 5.79939Z"
+                        fill="#1A202C"
+                        stroke="#1A202C"
+                        stroke-width="0.5"
+                      />
+                    </svg>
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
 }
 
 export default Pickswitchdrop;
-
-
